4be21f0e1c1b2f678a14388f68749ee6abfb1720
edwigepouhier <edwige.pouhier@live.fr>
2015-11-08 16:20:47 +0000

    Ruby + API

--- openfoodfacts.rb
+++ openfoodfacts.rb
@@ -1,1 +1,16 @@
+require 'open-uri'
+require 'json'
 
+loop do
+puts "Entrez un numéro de Code Barre :"
+bar_code = gets.chomp
+
+url = "http://fr.openfoodfacts.org/api/v0/produit/#{bar_code}.json"
+
+json = open(url).read
+
+data = JSONparse(json)
+
+puts data['product']['product_name']
+
+end
